a5c7f4637dfac77be5be356a2340da601f1364dc
Musawar Shah Syed <musawarbusiness@gmail.com>
2025-10-08 14:24:16 +0500

    Add Background Homepage Animations

--- client/src/Layout/Navbar.jsx
+++ client/src/Layout/Navbar.jsx
@@ -6,88 +6,71 @@ import { FiYoutube } from "react-icons/fi";
 import { NavLink } from "react-router";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-
-
 const Navbar = () => {
+  const [isMenuOpen, setisMenuOpen] = useState(false);
 
-  const [isMenuOpen ,setisMenuOpen] = useState(false)
   return (
     <div className="">
-
-    
-
-      
-
-        {/* Navbar on top of BG */}
-        <div className="relative z-10 flex h-20 items-center justify-around ">
-          <div>
-            <img src="" alt="LOGO" />
-          </div>
-
-          <div  className="flex gap-5 items-center text-2xl  ">
-            <NavLink className="hover:text-green-500 hidden md:block" to={"/"}>Home</NavLink>
-            <NavLink className="hover:text-green-500 hidden md:block" to={"/"}>Shop</NavLink>
-            <NavLink className="hover:text-green-500 hidden md:block" to={"/"}>About</NavLink>
-            <NavLink className="hover:text-green-500 hidden md:block" to={"/"}>Contact</NavLink>
-
-            <NavLink className=" hidden md:block " to={"/"}> <FiInstagram/> </NavLink>
-            <NavLink className=" hidden md:block " to={"/"}> <FaXTwitter/> </NavLink>
-            <NavLink className=" hidden md:block " to={"/"}> <CiFacebook/> </NavLink>
-            <NavLink className=" hidden md:block " to={"/"}> <FiYoutube/> </NavLink>
-            <NavLink className=" hidden md:block " to={"/"}> <FaCartShopping/> </NavLink>
-
-            <button onClick={() => setisMenuOpen(!isMenuOpen)} className='block md:hidden' >  <GiHamburgerMenu/>  </button>
-          </div>
-
-
-          
+      {/* Navbar */}
+      <div className="relative z-10 flex h-20 items-center justify-around bg-navbar">
+        <div>
+          <img src="" alt="LOGO" />
         </div>
 
-        {isMenuOpen ? (
-          <HamburgerModal/>
-        ): null}
+        <div className="flex gap-5 items-center text-2xl">
+          <NavLink className="text-navbar-text hover:text-navbar-hover hidden md:block" to={"/"}>Home</NavLink>
+          <NavLink className="text-navbar-text hover:text-navbar-hover hidden md:block" to={"/"}>Shop</NavLink>
+          <NavLink className="text-navbar-text hover:text-navbar-hover hidden md:block" to={"/"}>About</NavLink>
+          <NavLink className="text-navbar-text hover:text-navbar-hover hidden md:block" to={"/"}>Contact</NavLink>
 
-        {/* Hero Content */}
-        {/* <div className="relative z-10 flex flex-1 items-center justify-center">
-          <h1 className="text-5xl font-bold text-white">Hero Section</h1>
-        </div> */}
-        
-     
+          <NavLink className="hidden md:block" to={"/"}> <FiInstagram /> </NavLink>
+          <NavLink className="hidden md:block" to={"/"}> <FaXTwitter /> </NavLink>
+          <NavLink className="hidden md:block" to={"/"}> <CiFacebook /> </NavLink>
+          <NavLink className="hidden md:block" to={"/"}> <FiYoutube /> </NavLink>
+          <NavLink className="hidden md:block" to={"/"}> <FaCartShopping /> </NavLink>
 
-      {/* Section 2 */}
-      {/* <div className="h-screen bg-white flex items-center justify-center">
-        <h1 className="text-4xl text-black">Section 2 Content</h1>
-      </div> */}
+          <button
+            onClick={() => setisMenuOpen(!isMenuOpen)}
+            className="block md:hidden"
+          >
+            <GiHamburgerMenu />
+          </button>
+        </div>
+      </div>
 
+      {/* Animated Hamburger Menu */}
+      <div
+        className={`md:hidden transition-all duration-500 ease-in-out overflow-hidden transform ${
+          isMenuOpen
+            ? "max-h-96 opacity-100 translate-y-0"
+            : "max-h-0 opacity-0 -translate-y-5"
+        }`}
+      >
+        <HamburgerModal />
+      </div>
     </div>
-  )
-}
+  );
+};
 
+const HamburgerModal = () => {
+  return (
+    <div style={{paddingLeft:"5px"}} className=" flex flex-col gap-5 items-center text-1xl  bg-bg-secondary p-10 text-white md:hidden">
+      <div className="flex flex-col gap-3 text-start w-full">
+        <NavLink className="text-white hover:text-green-500  py-2 " to={"/"}>Home</NavLink>
+        <NavLink className="text-white hover:text-green-500  py-2 " to={"/"}>Shop</NavLink>
+        <NavLink className="text-white hover:text-green-500  py-2 " to={"/"}>About</NavLink>
+        <NavLink className="text-white hover:text-green-500  py-2 " to={"/"}>Contact</NavLink>
+      </div>
 
-
-const HamburgerModal = () =>{
-  return(
-    <div  className="flex flex-col gap-5 items-center text-2xl bg-black  p-24 md:hidden">
-
-           <div className='flex flex-col gap-3'>
-           <NavLink className="hover:text-green-500 " to={"/"}>Home</NavLink>
-            <NavLink className="hover:text-green-500 " to={"/"}>Shop</NavLink>
-            <NavLink className="hover:text-green-500 " to={"/"}>About</NavLink>
-            <NavLink className="hover:text-green-500 " to={"/"}>Contact</NavLink>
-           </div>
-
-            <div className='flex gap-3 '>
-            <NavLink className="  " to={"/"}> <FiInstagram/> </NavLink>
-            <NavLink className="  " to={"/"}> <FaXTwitter/> </NavLink>
-            <NavLink className="  " to={"/"}> <CiFacebook/> </NavLink>
-            <NavLink className="  " to={"/"}> <FiYoutube/> </NavLink>
-            <NavLink className="  " to={"/"}> <FaCartShopping/> </NavLink>
-
-           
-
-            </div>
+      <div className="flex gap-5 text-2xl justify-center  w-full">
+        <NavLink to={"/"}> <FiInstagram /> </NavLink>
+        <NavLink to={"/"}> <FaXTwitter /> </NavLink>
+        <NavLink to={"/"}> <CiFacebook /> </NavLink>
+        <NavLink to={"/"}> <FiYoutube /> </NavLink>
+        <NavLink to={"/"}> <FaCartShopping /> </NavLink>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,32 +2,43 @@ import React from 'react'
 import Navbar from '../Layout/Navbar'
 
 import BG2 from "../../public/images/spidermanBG.jpg"
+import Iridescence from '../Components/Animated/Ridescence'
+import TextType from '../Components/Animated/TextType'
 
 const HomePage = () => {
   return (
     <div className="relative flex flex-col justify-center min-h-screen overflow-x-hidden">
-    {/* NAVBAR */}
-    <Navbar />
-  
-    {/* Background */}
-    <div
-      className="fixed top-0 left-1/2 -translate-x-1/2 w-screen h-screen -z-10 bg-cover bg-center"
-      style={{ backgroundImage: `url(${BG2})` }}
-    ></div>
-  
-    {/* HERO */}
-    <div className="flex grow flex-col items-center text-center gap-2 relative justify-center">
-      <p className="text-3xl font-light">Welcome to Gamers Place</p>
-      <h1 className="text-4xl md:text-6xl -4xl font-bold">
-        Discover the Beauty <span className="text-green-500">Gaming</span>
-        <br /> at Your Fingertips
-      </h1>
-      <button className="text-2xl w-48 rounded-3xl h-12 bg-green-400 hover:bg-green-800 transition-all duration-300 ease-in-out">
-        Shop now
-      </button>
+      {/* NAVBAR */}
+      <Navbar />
+
+      {/* Background */}
+      <div
+        className="fixed top-0 left-1/2 -translate-x-1/2 w-screen h-screen -z-10 bg-cover bg-center bg-bg-dark"
+      // style={{ backgroundImage: `url(${BG2})` }}
+      >
+        <Iridescence
+          color={[1, 0, 1]}
+          mouseReact={false}
+          amplitude={0.1}
+          speed={1.0}
+        />
+
+      </div>
+
+      {/* HERO */}
+      <div className="flex grow flex-col items-center text-center gap-2 relative justify-center">
+        <p className="text-3xl font-light text-text-secondary">Welcome to Gamers Place</p>
+
+        <h1 className="text-4xl md:text-6xl -4xl font-bold text-text-primary">
+          Discover the Beauty <span className="text-accent-cyan">Gaming</span>
+          <br /> at Your Fingertips
+        </h1>
+        <button className="text-2xl w-48 rounded-3xl h-12 cursor-pointer bg-btn-primary hover:bg-btn-primary-hover transition-all duration-300 ease-in-out">
+          Shop now
+        </button>
+      </div>
     </div>
-  </div>
-  
+
   )
 }
 
