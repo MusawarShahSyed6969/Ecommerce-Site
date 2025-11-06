e60e600564a53831385c67f24f4316b488775f25
MusawaR <musawarbusiness@gmail.com>
2025-11-06 13:18:10 +0500

    auth fix

--- server/router/AuthRouter.js
+++ server/router/AuthRouter.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, getProfile } = require('../controllers/AuthController');
-const { protect } = require('../middleware/auth.js');
+const { protect } = require('../middleware/auth');
+
 
 router.post('/register', register);
 router.post('/login', login);
